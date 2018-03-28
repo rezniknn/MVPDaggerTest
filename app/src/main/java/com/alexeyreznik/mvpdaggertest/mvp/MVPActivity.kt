2277403b0476cbdb2e42d961461805cfcc9ff08d
Alexey Reznik <rezniknn@gmail.com>
2018-03-28 17:18:17 +0300

    Fix MVPActivity's onCreate() invocation syntax

--- app/src/main/java/com/alexeyreznik/mvpdaggertest/mvp/MVPActivity.kt
+++ app/src/main/java/com/alexeyreznik/mvpdaggertest/mvp/MVPActivity.kt
@@ -1,7 +1,6 @@
 package com.alexeyreznik.mvpdaggertest.mvp
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 
 abstract class MVPActivity<in V : MVPContract.View, out P : MVPContract.Presenter<V>, out C : MVPContract.Component<V, P>> : AppCompatActivity(), MVPContract.View {
@@ -12,8 +11,8 @@ abstract class MVPActivity<in V : MVPContract.View, out P : MVPContract.Presente
     protected abstract fun createComponent(): C
 
     @Suppress("UNCHECKED_CAST")
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         createComponent()
         presenter.attachView(this as V)
     }
